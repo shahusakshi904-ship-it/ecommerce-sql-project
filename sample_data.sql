85efe38ebb67c81c0719cc03bfc3e6dbbf245707
shahusakshi904-ship-it <shahusakshi904@gmail.com>
2025-12-07 17:49:13 +0530

    Create sample_data.sql

---
+++ sample_data.sql
@@ -0,0 +1,6 @@
+INSERT INTO Customers VALUES
+(1,'Sakshi','Sakshi@gmail.com'.'Nagpur','2024-01-01');
+INSERT INTO Products VALUES
+(101,'Smartphone',20000);
+INSERT INTO Orders VALUES
+(5001,1,'2024-02-15',20000);
